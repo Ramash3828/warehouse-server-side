0cb9ff411b58bc2e079153c1a978365b2f36eb2c
Ramash3828 <ramash3828@gmail.com>
2022-05-01 21:05:23 +0600

    imvantory manage page's data loaded successfully

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,8 +10,7 @@ app.use(cors());
 app.use(express.json());
 //Connect to Database
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
-const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.no9we.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.no9we.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -24,15 +24,31 @@ async function run() {
             .db("bicycleStore")
             .collection("product");
 
+        //Insert data from client
         app.post("/product", async (req, res) => {
             const product = req.body;
             const result = await productCollection.insertOne(product);
-            console.log(result);
             res.send({ success: "Product added Successfully" });
         });
+
+        //Load data from databse to client
         app.get("/product", async (req, res) => {
             const products = await productCollection.find({}).toArray();
             res.send(products);
+        });
+
+        // POST data by keys
+        app.get("/product/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productCollection.findOne(query);
+            res.send(product);
+        });
+
+        // Update quantity
+        app.put("/product", async (req, res) => {
+            const quantity = req.params.qty;
+            console.log(quantity);
         });
     } finally {
     }
