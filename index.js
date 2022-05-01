39270e94816b6cb23ed8ee1fa8e7e03168da3583
Ramash3828 <ramash3828@gmail.com>
2022-05-01 12:37:04 +0600

    database connected

--- index.js
+++ index.js
@@ -7,6 +7,26 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+//Connect to Database
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.USER}:${process.env.PASS}@cluster0.no9we.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+const run = async () => {
+    try {
+        await client.connect();
+        console.log("db connect");
+        const productCollection = client
+            .db("bicycleStore")
+            .collection("product");
+    } finally {
+    }
+};
+run().catch(console.dir());
 
 //Root
 app.get("/", (req, res) => {
