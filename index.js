fa1f4eb503e070df0be2d4a9d797526dc5593a9e
Ramash3828 <ramash3828@gmail.com>
2022-05-02 21:21:17 +0600

    jwt token generate

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,6 +10,24 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 //Connect to Database
+
+function verifyToken(req, res, next) {
+    let authHeaders = req.headers.authorization;
+
+    if (!authHeaders) {
+        return res.status(401).send({ message: "Unauthorized access" });
+    }
+    const token = authHeaders.split(" ")[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: "Forbidden access" });
+        }
+
+        // console.log("decoded", decoded);
+        // req.decoded = decoded;
+    });
+    next();
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.no9we.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -19,11 +38,33 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         await client.connect();
-        console.log("db connect");
         const productCollection = client
             .db("bicycleStore")
             .collection("product");
 
+        // Auth
+        app.post("/login", async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(
+                user,
+                process.env.ACCESS_TOKEN_SECRET,
+                {
+                    expiresIn: "1d",
+                }
+            );
+            res.send({ accessToken });
+        });
+
+        // Get My Items
+        app.get("/myitem", verifyToken, async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const cursor = productCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+        // Get All items
         app.get("/inventory/countItem", async (req, res) => {
             const page = parseInt(req.query.page);
             const qty = parseInt(req.query.quantity);
