ee7af4e5959eb9e1a8ba1930d48645935113696b
Ramash3828 <ramash3828@gmail.com>
2022-05-03 00:34:43 +0600

    code edit

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ function verifyToken(req, res, next) {
         }
 
         // console.log("decoded", decoded);
-        // req.decoded = decoded;
+
+        next();
     });
-    next();
 }
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.no9we.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
